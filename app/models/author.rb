70e5caf065dc1165a764beb36604fc7f3d32df1b
Brandon Yee <bjyee@andrew.cmu.edu>
2011-02-02 20:05:03 -0500

    Created Book, Publisher, Author, BookAuthor models

---
+++ app/models/author.rb
@@ -0,0 +1,7 @@
+class Author < ActiveRecord::Base
+    attr_accessible :first_name, :last_name
+	has_many :book_authors
+	has_many :books, :through => :book_authors
+	validates_presence_of :first_name, :last_name
+	named_scope :all, :order => "last_name, first_name"
+end
